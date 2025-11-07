ec13fae88c28e9ab5be5637dfeee2129ad342619
Greg Knowles <Gknowles37@gmail.com>
2025-11-07 21:52:32 +0000

    Add patch for deprecated numpy aliases
    Added compatibility patch for deprecated numpy aliases.

--- src/imuposer/smpl/parametricModel.py
+++ src/imuposer/smpl/parametricModel.py
@@ -1,7 +1,16 @@
 r"""
     SMPL/MANO/SMPLH parametric model. Modified from https://github.com/CalciferZh/SMPL.
 """
-
+# --- Patch for old chumpy expecting deprecated numpy aliases ---
+import numpy as np
+if not hasattr(np, 'bool'): np.bool = bool
+if not hasattr(np, 'int'): np.int = int
+if not hasattr(np, 'float'): np.float = float
+if not hasattr(np, 'complex'): np.complex = complex
+if not hasattr(np, 'object'): np.object = object
+if not hasattr(np, 'unicode'): np.unicode = str
+if not hasattr(np, 'str'): np.str = str
+# ---------------------------------------------------------------
 
 __all__ = ['ParametricModel']
 
